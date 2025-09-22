71c0ff2efdc2b22649908a932527a26dd861bc95
lucascconrado <lucascconrado@gmail.com>
2025-09-22 02:50:11 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const doutora1 = document.getElementById('doutora1');
+
+doutora1.addEventListener('mouseenter', () => {
+    doutora1.src = 'img/doutora2.webp';
+});
+doutora1.addEventListener('mouseleave', () => {
+    doutora1.src = 'img/doutora1.webp';
+});
